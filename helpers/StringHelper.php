b0b2be4b1b973f493c9ca189369ac8747df3a251
the-nepodarok <kosov.a@list.ru>
2024-02-03 21:34:07 +0300

    string helper class introduced

---
+++ helpers/StringHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\helpers;
+
+class StringHelper extends \yii\helpers\StringHelper
+{
+    public static function str_between($string, $start, $end): string
+    {
+        $string = ' ' . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return '';
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+
+        return substr($string, $ini, $len);
+    }
+}
