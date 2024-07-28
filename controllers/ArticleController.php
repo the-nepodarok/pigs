62bcf6863038d5ba3bb6791d89d2429dda91c804
the-nepodarok <kosov.a@list.ru>
2024-07-28 18:05:36 +0300

    news given full text editor

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -3,12 +3,9 @@
 namespace app\controllers;
 
 use app\models\Article;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Json;
 use app\models\Photo;
 use yii\web\BadRequestHttpException;
 use yii\web\NotFoundHttpException;
-use yii\web\UploadedFile;
 
 class ArticleController extends ApiController
 {
@@ -38,25 +35,14 @@ class ArticleController extends ApiController
         $newArticle->type_id = $type_id;
         $photos = [];
 
-        // если создаётся статья из полнотекстового редактора
-        if ($type_id === 1) {
-            $photos = $newArticle->handleImageMarkup($photos);
-        }
+        // обработать картинки из полнотекстового редактора
+        $photos = $newArticle->handleImageMarkup($photos);
 
         if (empty($newArticle->errors) and $newArticle->validate()) {
             $newArticle->save(false);
 
-            if ($newArticle->type_id === 1) {
-                foreach ($photos as $photo) {
-                    $newArticle->linkPhoto($photo);
-                }
-            } else {
-                // меняет порядок файлов, если одна из фотографий выбрана главной
-                if (isset($formData['main_photo_index']) && intval($formData['main_photo_index']) !== 0) {
-                    $newArticle->changePhotoOrder($formData['main_photo_index']);
-                }
-
-                $newArticle->handleNewPhotos();
+            foreach ($photos as $photo) {
+                $newArticle->linkPhoto($photo);
             }
 
             return $newArticle;
@@ -80,50 +66,22 @@ class ArticleController extends ApiController
 
                 $photos = [];
 
-                // если редактируется статья из полнотекстового редактора
-                if ($article->type_id === 1) {
-                    $photos = $article->handleImageMarkup($photos);
-                    $difference = $article->comparePhotos(array_column($photos, 'image'));
+                // обрабатываем картинки из полнотекстового редактора
+                $photos = $article->handleImageMarkup($photos);
+                $difference = $article->comparePhotos(array_column($photos, 'image')) ?? [];
 
-                } else {
-                    // Получаем уже имеющиеся фотографии
-                    $oldPhotos = Json::decode($formData['old_photos']);
-
-                    // Сравниваем фотографии с загруженными ранее
-                    $difference = $article->comparePhotos($oldPhotos);
-
-                    if ($difference) {
-
-                        // Удаляем лишние фотографии
-                        foreach ($difference as $filename) {
-                            try {
-                                $article->unlinkPhoto(Photo::find()->where(['image' => $filename])->one());
-                            } catch (\Exception $e) {
-                                error_log($e->getMessage());
-                            }
-                        }
-
-                        $article->refresh();
+                // Удаляем лишние фотографии
+                foreach ($difference as $filename) {
+                    try {
+                        $article->unlinkPhoto(Photo::find()->where(['image' => $filename])->one());
+                    } catch (\Exception $e) {
+                        error_log($e->getMessage());
                     }
                 }
 
                 // Загружаем новые из формы
-                if ($article->type_id === 1) {
-                    foreach ($photos as $photo) {
-                        $article->linkPhoto($photo);
-                    }
-                } else {
-
-                    // если одна из старых или новых фотографий должна стать главной
-                    if (isset($formData['main_photo_name']) || isset($formData['main_photo_index'])) {
-                        $mainPhotoName = $formData['main_photo_name'] ?? false;
-                        $mainPhotoIndex = $formData['main_photo_index'] ?? false;
-                        $article->rearrangePhotos($mainPhotoName, $mainPhotoIndex);
-
-                    } elseif ($article->files) {
-                        // если главной не была отмечена ни одна фотография, просто загрузить файлы
-                        $article->handleNewPhotos();
-                    }
+                foreach ($photos as $photo) {
+                    $article->linkPhoto($photo);
                 }
 
                 $article->save(false);
